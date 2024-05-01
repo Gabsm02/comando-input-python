f1272da74816c3382ff718948a071d4527a211f1
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-05-01 14:52:50 -0300

    Aprendendo sobre o comando input

---
+++ input.py
@@ -0,0 +1,8 @@
+# A funcao input() recebe um texto como argumento e retorna o que o usuario digitar
+
+# input("Digite algo: ")
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print("Seu nome é " + nome + " e você tem " + idade + " anos")
